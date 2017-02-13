448b29368331cdf4f736dbc72af90a25036a6001
Oren Yonash <OrenYonash@gmail.com>
2017-02-13 18:44:21 +0200

    Setup start script & heroku port

--- server4.js
+++ server4.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require(`hbs`); //require handlebars template enigne
 const fs = require('fs');
 
+const port = process.env.PORT || 3000; //use env var for online deployment
 var app = express(); 
 hbs.registerPartials(__dirname + '/views/partials');
 
@@ -56,7 +57,9 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 }); 
 
